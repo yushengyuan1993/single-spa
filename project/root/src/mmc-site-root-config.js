f8427a8eb01a08b5d9fc80e1435011727b423bae
yushengyuan <582718643@qq.com>
2022-06-11 20:41:44 +0800

    feat: 主应用

---
+++ project/root/src/mmc-site-root-config.js
@@ -0,0 +1,46 @@
+import { registerApplication, start } from "single-spa";
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+/**
+ * 默认的 welcome 应用
+ */
+const loadingWelcome = () => {
+  return System.import(
+    "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+  )
+}
+registerApplication(
+  '@single-spa/welcome',
+  loadingWelcome,
+  location => location.pathname === '/'
+);
+
+/**
+ * mmc-site-store 子应用
+ */
+registerApplication({
+  name: "@mmc-site/store",
+  app: () => System.import("@mmc-site/store"),
+  activeWhen: ["/store"]
+});
+
+/**
+ * mmc-site-report 子应用
+ */
+ registerApplication({
+  name: "@mmc-site/report",
+  app: () => System.import("@mmc-site/report"),
+  activeWhen: ["/report"]
+});
+
+start({
+  urlRerouteOnly: true,
+});
