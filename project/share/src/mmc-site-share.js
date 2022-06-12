1614bfd218e5c5050512eea484b467bdfbea86cb
yushengyuan <582718643@qq.com>
2022-06-12 16:53:10 +0800

    feat: share

---
+++ project/share/src/mmc-site-share.js
@@ -0,0 +1,4 @@
+// Anything exported from this file is importable by other in-browser modules.
+export const greet = (app) => {
+  console.log(`%cgreeting from ${app}`, 'color: #66b587');
+}
---
+++ project/share/webpack.config.js
@@ -0,0 +1,18 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "mmc-site",
+    projectName: "share",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 9003
+    }
+  });
+};
